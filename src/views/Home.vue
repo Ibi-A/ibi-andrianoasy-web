5344b8baa5fef654bd8e3d9c339c816c531b97a5
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-02 13:41:26 +0100

    homepage update with a jumbotrong reading data from local JSON

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <b-jumbotron :header="aboutMe.name" :lead="aboutMe.title"><p>{{ aboutMe.description }}</p></b-jumbotron>
   </div>
 </template>
 
 <script>
+import AboutMe from '../data/aboutMe.json'
+
 export default {
   components: {
     
+  },
+  data() {
+    return {
+      aboutMe: AboutMe
+    }
   }
 }
 </script>
