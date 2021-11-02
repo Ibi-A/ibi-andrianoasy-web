1f900bb5e1ca17b63862bb910457bedb35b070d2
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-02 15:02:29 +0100

    home page finalisation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <b-navbar fixed>
+    <b-navbar variant="light" sticky>
         <b-button v-b-toggle.sidebar-1 variant="outline"><b-icon icon="layout-sidebar"/></b-button>
         <b-navbar-brand href="#">Ibi Andrianoasy</b-navbar-brand>
     </b-navbar>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,41 @@
 <template>
   <div>
-    <b-jumbotron :header="aboutMe.name" :lead="aboutMe.title"><p>{{ aboutMe.description }}</p></b-jumbotron>
+    <b-container>
+      <b-row align-v="center" align-h="center">
+        <b-col xl="3"><b-img :src="aboutMe.profilePictureUrl" rounded="circle" fluid-grow/></b-col>
+        <b-col>
+          <b-jumbotron :header="aboutMe.name" :lead="aboutMe.title">
+            <hr/>
+            <p>{{ aboutMe.description }}</p>
+          </b-jumbotron>
+        </b-col>
+      </b-row>
+    </b-container>
+    
+    
+    <b-container>
+      <b-row>
+        <b-col v-for="contactInfo in contact" :key="contactInfo.id" xl="6">
+          <contact-card :name="contactInfo.name" :value="contactInfo.value"/>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import AboutMe from '../data/aboutMe.json'
+import Contact from '../data/contact.json'
+import ContactCard from '../components/ContactCard.vue'
 
 export default {
   components: {
-    
+    ContactCard,
   },
   data() {
     return {
-      aboutMe: AboutMe
+      aboutMe: AboutMe,
+      contact: Contact.values
     }
   }
 }
