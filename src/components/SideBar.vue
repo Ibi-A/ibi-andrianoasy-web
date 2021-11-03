4d942a2c52b4638d392dcf4d975a81803a7ca89a
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-03 17:40:13 +0100

    updated sidebar with icons

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,20 +2,19 @@
     <div>
         <b-sidebar
             id="sidebar-1"
-            title="Menu principal"
-            shadow="true"
+            title="Menu"
             backdrop
         >
             <b-nav vertical>
-                <b-nav-item href="/">À propos de moi</b-nav-item>
-                <b-nav-item href="/experience">Expérience professionnelle</b-nav-item>
-                <b-nav-item href="/diplomas">Formation</b-nav-item>
-                <b-nav-item href="/certifications">Certifications</b-nav-item>
-                <b-nav-item href="/activities">Activités annexes</b-nav-item>
-                <b-nav-item href="/expertises">Expertise</b-nav-item>
-                <b-nav-item href="/skills">Compétences</b-nav-item>
-                <b-nav-item href="/tools">Outils et méthodes</b-nav-item>
-                <b-nav-item href="/languages">Langues</b-nav-item>
+                <b-nav-item href="/"><b-icon icon="person" class="mr-3"/>À propos de moi</b-nav-item>
+                <b-nav-item href="/experience"><b-icon icon="briefcase" class="mr-3"/>Expérience professionnelle</b-nav-item>
+                <b-nav-item href="/diplomas"><b-icon icon="award" class="mr-3"/>Diplômes</b-nav-item>
+                <b-nav-item href="/certifications"><b-icon icon="patch-check" class="mr-3"/>Certifications</b-nav-item>
+                <b-nav-item href="/activities"><b-icon icon="star" class="mr-3"/>Activités annexes</b-nav-item>
+                <b-nav-item href="/expertises"><b-icon icon="list-check" class="mr-3"/>Expertise</b-nav-item>
+                <b-nav-item href="/skills"><b-icon icon="list" class="mr-3"/>Compétences</b-nav-item>
+                <b-nav-item href="/tools"><b-icon icon="tools" class="mr-3"/>Outils et méthodes</b-nav-item>
+                <b-nav-item href="/languages"><b-icon icon="globe" class="mr-3"/>Langues</b-nav-item>
             </b-nav>
         </b-sidebar>
     </div>
